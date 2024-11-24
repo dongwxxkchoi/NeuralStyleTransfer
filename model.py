9ef536f7ec7539c2aa47c027a2b4216448910437
dongwook <dwchoi0610@gmail.com>
2024-11-24 15:39:08 +0900

    🏗️ init StyleTransfer class

---
+++ model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+class StyleTransfer(nn.Module):
+    def __init__(self,):
+        super(StyleTransfer, self).__init__()
+        #TODO: VGG19 load
+        #TODO: separate conv layers
+    
+    def forward(self, x):
+        pass
